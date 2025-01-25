51e8e61560f335d8a78aaa346a57ecb2aca62b29
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2025-01-25 20:23:59 +0900

    アカウント関連のエンティティ、DTO、サービス、リゾルバーを削除し、GraphQLモジュールを設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import * as path from 'path';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    GraphQLModule.forRoot({
+      auutoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
